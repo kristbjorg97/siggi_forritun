a2e91aa119da91ab82ad23885dfb1ff9b4997fff
kristbjorg97 <54946715+kristbjorg97@users.noreply.github.com>
2020-01-30 15:23:38 +0000

    Update Fair_traffic_lights.py

--- Fair_traffic_lights.py
+++ Fair_traffic_lights.py
@@ -23,3 +23,5 @@ north_int = int(input("Number of cars travelling north: "))
 south_int = int(input("Number of cars travelling south: "))
 east_int = int(input("Number of cars travelling east: "))
 west_int = int(input("Number of cars travelling west: "))
+
+#halloo
